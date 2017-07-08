244f37a2f105bb09ab96e301141f1f4a53e8dd22
James Le <le_j6@denison.edu>
2017-07-07 22:59:37 -0700

    Initial Commit

---
+++ job-search.js
@@ -0,0 +1,7 @@
+/* You have a defined set of activities you can be doing during your job search
+process. Each activity has a cost (time that it takes you to complete the activity)
+and each activity provides some value 9Xp, or experience points, that will increase
+your chances of finding a job).
+
+Write a function that maximizes XP for a given input of time. Try to make your solution
+as efficient as possible. */
